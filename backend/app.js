b6127463c364b9863ddec4f458ebc69c64b1fe09
ilay ravia <ilayravia@gmail.com>
2020-07-03 14:48:34 +0300

    connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app= express();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology:true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
