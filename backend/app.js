6ab600104fb652e858786a04711af2e51f96e1f1
ilay ravia <ilayravia@gmail.com>
2020-07-05 11:19:39 +0300

    get all toraBooks and insert new torabook

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app= express();
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology:true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
+
+require('./models/ToraBook');
+
+app.use(bodyParser.json());
+app.use(require('./routes'));
 
 
 const port = process.env.PORT || 3000;
---
+++ backend/routes/api/toraBooks.js
@@ -0,0 +1,97 @@
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const ToraBook = mongoose.model('ToraBook');
+const hebcal = require('hebcal');
+
+
+router.get('/', async(req,res)=>{
+    var books = await ToraBook.find({});
+    res.status(200).json(books);
+})
+
+
+//for dev porpose
+router.post('/hasToday', async(req,res)=>{
+    const toraBook = new ToraBook({
+        name: req.body.name,
+        azcaraDates: [new hebcal.HDate()]
+    })
+    try {
+        const newBook = await toraBook.save();
+        res.status(201).json(newBook);
+
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({messge: err});
+    }
+});
+
+
+router.post('/', addNewToraBook);
+
+
+
+async function addNewToraBook(req,res,next){
+    let dates= req.body.azcaraDates;
+    let azcaraDates = [];
+    dates.forEach(date => {
+        let azcara = new hebcal.HDate(date.day,date.month,date.year); 
+        azcaraDates.push(azcara);
+    });
+
+    const toraBook = new ToraBook({
+        name: req.body.name,
+        azcaraDates: azcaraDates
+    });
+
+    try {
+        const newBook = await toraBook.save();
+        res.status(201).json(newBook);
+
+    } catch (err) {
+        res.status(400).json({messge: err.messge})
+    }
+}
+
+router.post('/hasToday', async(req,res)=>{
+    const toraBook = new ToraBook({
+        name: req.body.name,
+        azcaraDates: [new hebcal.HDate()]
+    })
+    try {
+        const newBook = await toraBook.save();
+        res.status(201).json(newBook);
+
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({messge: err});
+    }
+});
+
+async function getSeferHasAzcara(req,res,next) {
+    const today = new hebcal.HDate();
+    let week = [];
+    let day = today;
+    for (let index = 0; index < 7; index++) {
+        week.push(day);
+        day = day.next();
+    }
+    try {
+        //find all the books that has azcara next Week
+        let booksWithAzcaraThisWeek = [];
+        for (const d of week) {
+            let booksWithAzcara = await ToraBook.find({ azcaraDates: d });
+            booksWithAzcaraThisWeek.push(booksWithAzcara);
+        }
+        bookWithAzcaraThisWeek = booksWithAzcaraThisWeek.flat(1);
+        res.book = bookWithAzcaraThisWeek;
+    }
+    catch (err) {
+        res.status(500).json({ messge: err.messge });
+    }
+    next();
+}
+
+
+
+module.exports = router;
