3dd01e1d57b7f87623c9504b98d65ab20acb4262
ilay ravia <ilayravia@gmail.com>
2020-07-05 11:11:06 +0300

    add ToraBook scheme

---
+++ backend/models/ToraBook.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+var ToraBookScheme = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    azcaraDates:{
+        type: Array
+    },
+    usageScore:{
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('ToraBook',ToraBookScheme);
